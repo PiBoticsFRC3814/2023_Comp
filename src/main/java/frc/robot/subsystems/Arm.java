658bbeac0069ae7cd17ee5f44824bcf9fd792561
pibotics3814 <pibotics3814@gmail.com>
2023-02-07 16:27:33 -0800

    ba

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -4,9 +4,15 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Arm extends SubsystemBase {
+  private static final String TalonSRX = null;
+
   /** Creates a new Arm. */
   public Arm() {}
 
@@ -29,8 +35,7 @@ public class Arm extends SubsystemBase {
 
   private void ArmRetract() {
     //Arm (Retract)
-  }  
-
+  }
 }
 
 
