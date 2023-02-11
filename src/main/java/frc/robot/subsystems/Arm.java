015ea3e7aac161d96edb48d0f899a3a1d2130918
pibotics3814 <pibotics3814@gmail.com>
2023-02-11 12:30:37 -0500

    ArmAngle statements - af

--- src/main/java/frc/robot/subsystems/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm.java
@@ -6,6 +6,11 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj2.command.PIDCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -19,17 +24,33 @@ public class Arm extends SubsystemBase {
   public boolean extendAtPos;
   public boolean shoulderAtPos;
 
+  DoubleSolenoid armBrake = new DoubleSolenoid(PneumaticsModuleType.CTREPCM, Constants.CLAW_ID_OPEN, Constants.CLAW_ID_CLOSE);
+
+  PIDController angleController;
+
   public Arm() {
     shoulder = new WPI_TalonSRX(Constants.SHOULDER_ID);
     extend = new WPI_TalonSRX(Constants.EXTEND_ID);
+
+    angleController = new PIDController(0, 0, 0);
   }
 
   public void ArmAngle(double angle) {
+    double encoder = 0.0;
+
+    shoulder.set(angleController.calculate(encoder, angle));
+    shoulderAtPos = !angleController.atSetpoint();
+    
+    if(!shoulderAtPos) 
+      armBrake.set(DoubleSolenoid.Value.kReverse);
+    else
+      armBrake.set(DoubleSolenoid.Value.kForward);
     
   }
 
   public void ArmDistance(double distance) {
     //Arm (Extend)
+
   }
 }
 
