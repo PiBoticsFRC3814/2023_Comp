d5bc6b0ee5b55161168eaf07baed102e7d83d9fc
Eragon3650 <eragon3650@gmail.com>
2023-02-13 10:59:30 -0500

    cleaned up GyroSwerveDrive - AH

--- src/main/java/frc/robot/subsystems/GyroSwerveDrive.java
+++ src/main/java/frc/robot/subsystems/GyroSwerveDrive.java
@@ -7,74 +7,58 @@ import edu.wpi.first.wpilibj.ADIS16470_IMU;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class GyroSwerveDrive extends SubsystemBase {
-  public double[] speed = new double[4];
-  public double[] angle = new double[4];
-  public boolean  fcd = true;
+  private double[] speed = {0.0, 0.0, 0.0, 0.0};
+  private double[] angle = {0.0, 0.0, 0.0, 0.0};
 
-  private SwerveModule[] swerveMod = {new SwerveModule(0), new SwerveModule(1), new SwerveModule(2), new SwerveModule(3)};
+  private SwerveModule[] swerveMod = {
+    new SwerveModule(0),
+      new SwerveModule(1),
+        new SwerveModule(2),
+          new SwerveModule(3)
+  };
 
   ADIS16470_IMU m_gyro;
 
-  public void gyroDrive( double str, double fwd, double rot, double gyroAngle ) {
-    
-    computeSwerveInputs( str, fwd, rot, gyroAngle );
-    setSetpoints( rot ); 
+  public GyroSwerveDrive(){}
 
-    for(int i = 0; i < 4; i++) {
+  public void gyroDrive( double str, double fwd, double rot, double gyroAngle ) {
+    double intermediary = fwd * Math.cos( gyroAngle ) + str * Math.sin( gyroAngle );
+    str = -fwd * Math.sin( gyroAngle ) + str * Math.cos( gyroAngle );
+
+    computeSwerveInputs( str, intermediary, rot);
+
+    for(int i = 0; i < 4; i++){
       speed[i] *= Constants.MAX_SPEED_JOYSTICK;
-      swerveMod[i].drive( speed[i], angle[i] );
     }
+
+    setSetpoints();
   }
 
   public void drive(double str, double fwd, double rot) {
-    double a = str - rot * 1.5 * ( Constants.SWERVE_FRAME_LENGTH / Constants.SWERVE_RADIUS );
-    double b = str + rot * 1.5 * ( Constants.SWERVE_FRAME_LENGTH / Constants.SWERVE_RADIUS );
-    double c = fwd - rot * 1.5 * ( Constants.SWERVE_FRAME_WIDTH / Constants.SWERVE_RADIUS );
-    double d = fwd + rot * 1.5 * ( Constants.SWERVE_FRAME_WIDTH / Constants.SWERVE_RADIUS );
-    
-    speed[1] = Math.sqrt( ( a * a ) + ( d * d ) );
-    speed[2] = Math.sqrt( ( a * a ) + ( c * c ) );
-    speed[0] = Math.sqrt( ( b * b ) + ( d * d ) );
-    speed[3] = Math.sqrt( ( b * b ) + ( c * c ) );
-
-    angle[1] = Math.atan2 ( a, d ) / Math.PI;
-    angle[2] = Math.atan2 ( a, c ) / Math.PI;
-    angle[0] = Math.atan2 ( b, d ) / Math.PI;
-    angle[3] = Math.atan2 ( b, c ) / Math.PI;
-    setSetpoints(rot); 
-    for (int i = 0; i < 4; i++) {
-        swerveMod[i].drive( speed[i], angle[i] );
-    }
+    computeSwerveInputs(str, fwd, rot);
+    setSetpoints();
   }
 
   /* 
    * Brake system
    */
   public void brakeAngle() {
-    swerveMod[0].drive(0.25, 0.0);
-    swerveMod[1].drive(-0.25, 0.0);
-    swerveMod[2].drive(0.25, 0.0);
-    swerveMod[3].drive(-0.25, 0.0);
-
-  }
-
-  public void autoDrive( double angle, double speed ) {
-    for (int i = 0; i < 4; i++) {
-      swerveMod[i].drive( angle, speed );
-    }
+    angle[0] = 0.25;
+    angle[1] = -0.25;
+    angle[2] = 0.25;
+    angle[3] = -0.25;
+    speed[0] = 0.0;
+    speed[1] = 0.0;
+    speed[2] = 0.0;
+    speed[3] = 0.0;
+    setSetpoints();
   }
 
   private double getDeltaAngle( double alpha, double beta){
     return 1.0 - Math.abs(Math.abs(alpha - beta) % 2.0 - 1.0);
   }
 
-  public void computeSwerveInputs( double str, double fwd, double rot, double gyroAngle ) {
-    if(fcd){
-      double intermediary = fwd * Math.cos( gyroAngle ) + str * Math.sin( gyroAngle );
-      str = -fwd * Math.sin( gyroAngle ) + str * Math.cos( gyroAngle );
-      fwd = intermediary;
-    }
-
+  private void computeSwerveInputs( double str, double fwd, double rot) {
     double a = str - rot * ( Constants.SWERVE_FRAME_LENGTH / Constants.SWERVE_RADIUS );
     double b = str + rot * ( Constants.SWERVE_FRAME_LENGTH / Constants.SWERVE_RADIUS );
     double c = fwd - rot * ( Constants.SWERVE_FRAME_WIDTH / Constants.SWERVE_RADIUS );
@@ -91,16 +75,14 @@ public class GyroSwerveDrive extends SubsystemBase {
     angle[3] = Math.atan2( b, c ) / Math.PI;
   }
 
-  public void setSetpoints( double rot ) {
+  private void setSetpoints() {
     for(int i = 0; i < 4; i++) {
       double steerAngle = swerveMod[i].getSteerAngle();
       if(getDeltaAngle(angle[i], steerAngle) > 0.5) {
         angle[i] =  Math.abs(Math.abs(angle[i] + 2.0) % 2.0) - 1.0;
         speed[i] = -speed[i];
       }
-
-      SmartDashboard.putNumber( "angle: " + i, angle[i] );
-      SmartDashboard.putNumber( "speed: " + i, speed[i] );
+      swerveMod[i].drive( speed[i], angle[i] );
     }
   }
 
@@ -113,32 +95,6 @@ public class GyroSwerveDrive extends SubsystemBase {
   public void WheelToBrake(){
     for(int i = 0; i < 4; i++){
        swerveMod[i].driveMotor.setIdleMode( IdleMode.kBrake );
-    }
-  }
-
-  public void driveStraight( double fwd ) {
-    double a = 0;
-    double b = 0;
-    double c = -fwd;
-    double d = -fwd;
-    
-    speed[1] = Math.sqrt( d * d );
-    speed[2] = Math.sqrt( c * c );
-    speed[0] = Math.sqrt( d * d );
-    speed[3] = Math.sqrt( c * c );
-
-    angle[1] = Math.atan2( a, d ) / Math.PI;
-    angle[2] = Math.atan2( a, c ) / Math.PI;
-    angle[0] = Math.atan2( b, d ) / Math.PI;
-    angle[3] = Math.atan2( b, c ) / Math.PI;
-
-    for(int i = 0; i < 4; i++){
-      double steerAngle = swerveMod[i].getSteerAngle();
-      if(getDeltaAngle(angle[i], steerAngle) > 0.5) {
-        angle[i] =  Math.abs(Math.abs(angle[i] + 2.0) % 2.0) - 1.0;
-        speed[i] = -speed[i];
-      }
-      swerveMod[i].drive( speed[i], angle[i] );
     }
   }
 
